eccbb97e7f9f3eb3eac965e38f413c76f4de6f3a
m <marcin.swistun@gmail.com>
2020-09-23 21:10:59 +0200

    project init

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : whistlers-state-machine.c
+ Author      : marcin.swistun@gmail.com
+ Version     :
+ Copyright   :
+ Description : State machine automation
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("init check");
+	return EXIT_SUCCESS;
+}
